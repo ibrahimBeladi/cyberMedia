33f032556d7b9daaff210918bff6f4dff18682e5
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-08 12:12:41 +0300

    extracted i18n

---
+++ i18n.js
@@ -0,0 +1,17 @@
+var messages = {
+    en: {
+        message: {
+            toBe: 'To be or not to be'
+        }
+    },
+    ar: {
+        message: {
+            toBe: 'اكون او لا اكون'
+        }
+    }
+};
+
+var i18n = new VueI18n({
+    locale: 'en',
+    messages
+});
