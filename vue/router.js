c9951e29bd3a7972f63768af90d926b930d109cf
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-20 13:50:43 +0300

    transition

--- vue/router.js
+++ vue/router.js
@@ -4,5 +4,8 @@ var router = new VueRouter({
 		{ path: '/home', name: 'Home', component: homeRoute },
 		{ path: '/media', name: 'Media', component: mediaRoute },
 		{ path: '/team', name: 'Team', component: teamRoute },
-	]
+	],
+	scrollBehavior(to, from, savedPosition) {
+		return { x: 0, y: 0 }
+	}
 });
