4f4d483fbfb565301ce4a34d30ab71b67d71f995
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-20 13:50:56 +0300

    redirect

--- vue/router.js
+++ vue/router.js
@@ -1,6 +1,6 @@
 var router = new VueRouter({
 	routes: [
-		{ path: '/', name: 'Base', component: homeRoute },
+		{ path: '/', redirect: '/home' },
 		{ path: '/home', name: 'Home', component: homeRoute },
 		{ path: '/media', name: 'Media', component: mediaRoute },
 		{ path: '/team', name: 'Team', component: teamRoute },
