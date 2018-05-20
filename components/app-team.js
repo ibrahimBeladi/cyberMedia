c340cd30ab153450945d81020bc5fb764bfcaea9
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-20 11:35:39 +0300

    ..

--- components/app-team.js
+++ components/app-team.js
@@ -8,7 +8,7 @@ Vue.component("app-team", {
 				</span>
 			</v-card-title>
 			<v-card-text class="pa-1">
-				<v-container fill-height grid-list-xl>
+				<v-container grid-list-md fill-height :class="getClass">
 					<v-layout row wrap justify-center>
 						<v-flex xs6 md4 v-for="p in team" :key="p.name">
 							<v-card>
@@ -45,6 +45,15 @@ Vue.component("app-team", {
 				case "md": return "150px";
 				case "lg": return "200px";
 				case "xl": return "250px";
+			}
+		},
+		getClass: function () {
+			switch (this.$vuetify.breakpoint.name) {
+				case "xs": return "pa-0";
+				case "sm": return "pa-1";
+				case "md": return "pa-2";
+				case "lg": return "pa-3";
+				case "xl": return "pa-4";
 			}
 		}
 	}
