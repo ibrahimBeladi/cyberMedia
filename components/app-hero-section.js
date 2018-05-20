66ab90ff817409e321f77aed4fcf60b6070a9513
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-20 14:23:41 +0300

    stylin

--- components/app-hero-section.js
+++ components/app-hero-section.js
@@ -1,6 +1,6 @@
 Vue.component("app-hero-section", {
-	template: `
-        <v-parallax src="./res/imgs/parallax.jpg" height="500">
+    template: `
+        <v-parallax src="./res/imgs/parallax.jpg" :height="parallaxHeight">
             <v-carousel delimiter-icon="mdi-stop" interval="7000" hide-controls>
                 <v-carousel-item v-for="(item,i) in 2" :key="i">
                     <v-layout fill-height class="ma-0">
@@ -8,10 +8,10 @@ Vue.component("app-hero-section", {
                             <v-container>
                                 <v-card dark flat style="background-color: rgba(0,0,0,0);">
                                     <v-card-text class="pa-0 text-xs-center">
-                                        <span class="display-3">
+                                        <span :class="quoteSize">
                                             "{{ $t("message.quote"+(i+1)) }}"
                                         </span>
-                                        <span class="display-1" v-html="$t('message.quoter')"></span>
+                                        <span :class="quoterSize" v-html="$t('message.quoter')"></span>
                                     </v-card-text>
                                 </v-card>
                             </v-container>
@@ -20,5 +20,34 @@ Vue.component("app-hero-section", {
                 </v-carousel-item>
             </v-carousel>
         </v-parallax>
-	`,
+    `,
+    computed: {
+        parallaxHeight: function () {
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs": return "300";
+                case "sm": return "350";
+                case "md": return "400";
+                case "lg": return "500";
+                case "xl": return "500";
+            }
+        },
+        quoteSize: function(){
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs": return "heading larger";
+                case "sm": return "display-1";
+                case "md": return "display-2";
+                case "lg": return "display-3";
+                case "xl": return "display-3";
+            }
+        },
+        quoterSize: function(){
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs": return "subheading";
+                case "sm": return "heading";
+                case "md": return "display-1";
+                case "lg": return "display-1";
+                case "xl": return "display-1";
+            }
+        }
+    }
 });
