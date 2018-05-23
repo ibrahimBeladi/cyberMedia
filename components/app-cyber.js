40ee7eb76346054bafab7bc168c978e9b868ee9d
Ibrahim Beladi <ibra5him@hotmail.com>
2018-05-23 13:33:16 +0300

    app-cyber

--- components/app-cyber.js
+++ components/app-cyber.js
@@ -1,56 +1,64 @@
 Vue.component("app-cyber", {
 	template: `
 		<v-card class="py-5 grey lighten-4">
-			<v-card-title class="justify-center pt-5">
-				<span class="headline">
-					{{ $t( "cyber.cybersecurity" ) }}
-				</span>
-			</v-card-title>
-			<v-card-text class="pa-1">
-				<v-container grid-list-md :class="getClass">
-					<v-layout row wrap justify-center>
-						<v-flex xs12 v-for="question,i in cyberQuestions" :key="i">
-							<v-card>
-								<v-card-title class="pb-0">
-									<span class="title">
-										{{ $t( "cyber.cyber.q"+(i+1) ) }}
-									</span>
-								</v-card-title>
-								<v-card-text>
-									<span class="subheading">
-										{{ $t( "cyber.cyber.a"+(i+1) ) }}
-									</span>
-								</v-card-text>
-							</v-card>
-						</v-flex>
-					</v-layout>	
-				</v-container>
-			</v-card-text>
-			<v-card-title class="justify-center pt-5">
-				<span class="headline">
-					{{ $t( "cyber.types" ) }}
-				</span>
-			</v-card-title>
-			<v-card-text class="pa-1">
-				<v-container grid-list-md :class="getClass">
-					<v-layout row wrap justify-center>
-						<v-flex xs12 v-for="question,i in typeQuestions" :key="i">
-							<v-card>
-								<v-card-title class="pb-0">
-									<span class="title">
-										{{ $t( "cyber.type.t"+(i+1) ) }}
-									</span>
-								</v-card-title>
-								<v-card-text>
-									<span class="subheading">
-										{{ $t( "cyber.type.d"+(i+1) ) }}
-									</span>
-								</v-card-text>
-							</v-card>
-						</v-flex>
-					</v-layout>	
-				</v-container>
-			</v-card-text>
+			<v-container>
+				<v-expansion-panel>
+					<v-expansion-panel-content expand-icon="mdi-chevron-down" value="true" :class="{'forceRTL':$i18n.locale=='ar'}">
+						<div slot="header" class="title">
+							{{ $t( "cyber.cybersecurity" ) }}
+						</div>
+						<v-card>
+							<v-card-text class="pa-1">
+								<v-container grid-list-md :class="getClass">
+									<v-layout row wrap justify-center>
+										<v-flex xs12 v-for="question,i in cyberQuestions" :key="i">
+											<v-card class="elevation-2">
+												<v-card-title class="pb-0">
+													<span class="title">
+														{{ $t( "cyber.cyber.q"+(i+1) ) }}
+													</span>
+												</v-card-title>
+												<v-card-text>
+													<span class="subheading">
+														{{ $t( "cyber.cyber.a"+(i+1) ) }}
+													</span>
+												</v-card-text>
+											</v-card>
+										</v-flex>
+									</v-layout>	
+								</v-container>
+							</v-card-text>
+						</v-card>
+					</v-expansion-panel-content>
+					<v-expansion-panel-content expand-icon="mdi-chevron-down" :class="{'forceRTL':$i18n.locale=='ar'}">
+						<div slot="header" class="title">
+							{{ $t( "cyber.types" ) }}
+						</div>
+						<v-card>
+							<v-card-text class="pa-1">
+								<v-container grid-list-md :class="getClass">
+									<v-layout row wrap justify-center>
+										<v-flex xs12 v-for="question,i in typeQuestions" :key="i">
+											<v-card class="elevation-2">
+												<v-card-title class="pb-0">
+													<span class="title">
+														{{ $t( "cyber.type.t"+(i+1) ) }}
+													</span>
+												</v-card-title>
+												<v-card-text>
+													<span class="subheading">
+														{{ $t( "cyber.type.d"+(i+1) ) }}
+													</span>
+												</v-card-text>
+											</v-card>
+										</v-flex>
+									</v-layout>	
+								</v-container>
+							</v-card-text>
+						</v-card>
+					</v-expansion-panel-content>
+				</v-expansion-panel>
+			</v-container>	
 		</v-card>
 	`,
 	data: function () {
--- vue/i18n.js
+++ vue/i18n.js
@@ -46,18 +46,18 @@ var i18n = new VueI18n({
 					q2: "How does cybersecurity work?",
 					a2: "A successful cybersecurity approach has multiple layers of protection spread across the computers, networks, programs, or data that one intends to keep safe. In an organization, the people, processes, and technology must all complement one another to create an effective defense from cyber-attacks.",
 					q3: "Why is cybersecurity important?",
-					a3: "In today’s connected world, everyone benefits from advanced cyber defense programs. At an individual level, a cybersecurity attack can result in everything from identity theft, to extortion attempts, to the loss of important data like family photos. Everyone relies on critical infrastructure like power plants, hospitals, and financial service companies. Securing these and other organizations is essential to keeping our society functioning. Everyone also benefits from the work of cyber threat researchers, like the team of 250 threat researchers at Talos, who investigate new and emerging threats and cyber-attack strategies. They reveal new vulnerabilities, educate the public on the importance of cybersecurity, and strengthen open source tools. Their work makes the Internet safer for everyone.",
-					q4: "What are the consequences of a cyber-attack?",
+					a3: "In today’s connected world, everyone benefits from advanced cyber defense programs. At an individual level, a cybersecurity attack can result in everything from identity theft, to extortion attempts, to the loss of important data like family photos. Everyone relies on critical infrastructure like power plants, hospitals, and financial service companies. Securing these and other organizations is essential to keeping our society functioning.",
+					q4: "What are the consequences of cyber-attacks?",
 					a4: "Cyber-attacks can disrupt and cause considerable financial and reputational damage to even the most resilient organization. If you suffer a cyber-attack, you stand to lose assets, reputation and business, and potentially face regulatory fines and litigation – as well as the costs of remediation.",
 				},
 				types: "Types of cybersecurity threats",
 				type: {
-					t1: "Ransomware",
-					d1: "Ransomware is a type of malicious software. It is designed to extort money by blocking access to files or the computer system until the ransom is paid. Paying the ransom does not guarantee that the files will be recovered or the system restored.",
+					t3: "Ransomware",
+					d3: "Ransomware is a type of malicious software. It is designed to extort money by blocking access to files or the computer system until the ransom is paid. Paying the ransom does not guarantee that the files will be recovered or the system restored.",
 					t2: "Malware",
 					d2: "Malware is a type of software designed to gain unauthorized access or to cause damage to a computer. ",
-					t3: "Social engineering",
-					d3: "Social engineering is a tactic that adversaries use to trick you into revealing sensitive information. They can solicit a monetary payment or gain access to your confidential data. Social engineering can be combined with any of the threats listed above to make you more likely to click on links, download malware, or trust a malicious source.",
+					t1: "Social engineering",
+					d1: "Social engineering is a tactic that adversaries use to trick you into revealing sensitive information. They can solicit a monetary payment or gain access to your confidential data. Social engineering can be combined with any of the threats listed below to make you more likely to click on links, download malware, or trust a malicious source.",
 					t4: "Phishing",
 					d4: "Phishing is the practice of sending fraudulent emails that resemble emails from reputable sources. The aim is to steal sensitive data like credit card numbers and login information. It’s the most common type of cyber-attack. You can help protect yourself through education or a technology solution that filters malicious emails.",
 				}
@@ -100,6 +100,30 @@ var i18n = new VueI18n({
 				years: "سنين",
 				infinity: "infinity",
 				hack: "* على جهاز هكر عادي."
+			},
+			cyber: {
+				cybersecurity: "الأمن السيبراني",
+				cyber: {
+					q1: "ما هو الأمن السيبراني؟",
+					a1: "الأمن السيبراني هو ممارسة حماية الأنظمة والشبكات والبرامج من الهجمات الرقمية. وعادةً ما تهدف هذه الهجمات إلى الوصول إلى المعلومات الحساسة أو تغييرها أو إتلافها ؛ أو ابتزاز الأموال من المستخدمين ؛ أو مقاطعة العمليات التجارية العادية.",
+					q2: "كيف يعمل الأمن السيبراني؟",
+					a2: "يمتلك نهج الأمن السيبراني الناجح طبقات متعددة من الحماية تنتشر عبر أجهزة الكمبيوتر أو الشبكات أو البرامج أو البيانات التي ينوي المرء الحفاظ على سلامتها. في أي منظمة ، يجب على الأشخاص والعمليات والتكنولوجيا أن يكملوا بعضهم البعض لإنشاء فعالية الدفاع عن الهجمات السيبرانية. ",
+					q3: "لماذا يعد الأمان الإلكتروني مهمًا؟",
+					a3: "في عالم اليوم المترابط ، يستفيد الجميع من برامج الدفاع السيبراني المتقدمة. على المستوى الفردي ، يمكن أن يؤدي هجوم الأمن الإلكتروني إلى كل شيء من سرقة الهوية ، إلى محاولات الابتزاز ، إلى فقدان البيانات المهمة مثل الصور العائلية. يعتمد الجميع على البنية التحتية مثل محطات الطاقة والمستشفيات وشركات الخدمات المالية، لذا فإن تأمين هذه المنظمات وغيرها أمر ضروري للحفاظ على عمل مجتمعنا. ",
+					q4: "ما هي عواقب الهجمات الإلكترونية؟",
+					a4: "يمكن للهجمات الإلكترونية أن تسبب في قدر كبير من الضرر المالي والسمعي حتى لأكثر المنظمات مرونة. إذا كنت تعاني من هجوم عبر الإنترنت ، فإنك ستفقد الأصول والسمعة والأعمال التجارية ، وربما تواجه غرامات قانونية وتقاضي - وكذلك تكاليف الإصلاح. ",
+				},
+				types: "أنواع تهديدات الأمن السيبراني",
+				type: {
+					t3: "برنامج الفدية",
+					d3: "برنامج الفدية هو نوع من البرامج الضارة. وهو مصمم لابتزاز المال عن طريق منع الوصول إلى الملفات أو نظام الكمبيوتر حتى يتم دفع الفدية. دفع الفدية لا يضمن استرداد الملفات أو استعادة النظام.",
+					t2: "البرامج الضارة",
+					d2: "تعد البرامج الضارة نوعًا من البرامج المصممة للوصول غير المصرح به أو لإحداث ضرر بجهاز الكمبيوتر.",
+					t1: "الهندسة الاجتماعية",
+					d1: "الهندسة الاجتماعية هي تكتيك يستخدمه الخصوم لخداعك للكشف عن معلومات حساسة. يمكنهم طلب دفعات مالية أو الوصول إلى بياناتك السرية. يمكن دمج الهندسة الاجتماعية مع أي من التهديدات المذكورة أدناه لجعلك أكثر احتمالا للنقر على روابط أو تنزيل برامج ضارة أو الوثوق بمصدر ضار. ",
+					t4: "التصيد الاحتيالي",
+					d4: "التصيد هو ممارسة إرسال رسائل بريد إلكتروني مزورة تشبه رسائل البريد الإلكتروني من مصادر موثوقة. الهدف هو سرقة بيانات حساسة مثل أرقام بطاقات الائتمان ومعلومات تسجيل الدخول. وهو أكثر أنواع الهجمات الإلكترونية شيوعًا. يمكنك المساعدة في حماية نفسك من خلال التعليم أو حل تكنولوجي يعمل على تصفية الرسائل الإلكترونية الضارة. ",
+				}
 			}
 		}
 	}
